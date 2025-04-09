a197dbb282ffda2e539ce712427cf14bd68df274
Mohammad Hossein Fakouri <m.fakoori9776@gmail.com>
2025-04-09 23:19:50 +0330

    Update primenumber.py

--- primenumber.py
+++ primenumber.py
@@ -16,3 +16,19 @@ if prime:
     print ('prime')
 else:
     print ('not prime')
+
+
+def isprime(n):
+    prime = True
+    for i in range (2, n):
+        if n % i == 0:
+            prime = False
+    return prime
+
+if prime:
+    print ('prime')
+else:
+    print ('not prime')
+
+
+isprime(42)
