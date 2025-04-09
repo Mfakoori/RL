b54d1a16c75149e43902c5fc6196e393041d8cb7
Mohammad Hossein Fakouri <m.fakoori9776@gmail.com>
2025-04-09 23:12:46 +0330

    simple loop to check prime numbers

---
+++ primenumber.py
@@ -0,0 +1,18 @@
+for i in range (2, 8):
+    print (8/i)
+
+for i in range (2, 8):
+    if 8 % i == 0:
+        print ('because of ', i, ' not prime')
+
+
+n = 19  # input
+prime = True
+for i in range (2, n):
+    if n % i == 0:
+        prime = False
+
+if prime:
+    print ('prime')
+else:
+    print ('not prime')
