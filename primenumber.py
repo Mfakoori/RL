5f80cbe67dfb97fb4ce0037ae6be0fb50589a4c3
Mohammad Hossein Fakouri <m.fakoori9776@gmail.com>
2025-04-10 12:53:16 +0330

    optimization by reducing number of calculations

--- primenumber.py
+++ primenumber.py
@@ -23,7 +23,7 @@ print ('NEXT')
 
 def isprime(n):
     prime = True
-    for i in range (2, n):
+    for i in range (2, int(n**.5) + 1):
         if n % i == 0:
             prime = False
     return prime
