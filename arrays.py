aac759a2f94017fba6e1c0827e746a9a1e806bd6
Mohammad Hossein Fakouri <m.fakoori9776@gmail.com>
2025-04-15 16:30:52 +0330

    arrays examples

---
+++ arrays.py
@@ -0,0 +1,13 @@
+a = []
+print (type (a))
+b = [1, 2, 3]
+print ('b[0] = ',b[0])
+names = ['me', 'dad', 'mother', 'friend']
+print (names)
+names.append('john')
+names[2] = 'sister'
+print (names)
+print (len(names))
+print (names[0:2])
+print (names.count('john'))
+
