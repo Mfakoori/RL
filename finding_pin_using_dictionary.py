7a7f6eea1388c5d6ac4ddba7cf2a47b4face606f
Mohammad Hossein Fakouri <m.fakoori9776@gmail.com>
2025-04-17 16:12:43 +0330

    Update finding_pin_using_dictionary.py

--- finding_pin_using_dictionary.py
+++ finding_pin_using_dictionary.py
@@ -1,10 +1,10 @@
 def guess_is_correct(digits):
-    if digits['3rd'] + digits ['5th'] == 14 and\
-    digits['1st'] == 2*digits[2] - 1 and\
-    digits['4th'] == digits[2] + 1 and\
-    digits['2nd'] + digits['3rd'] == 10 and\
-    digits['1st'] + digits['2nd'] + digits['3rd'] + digits['4th'] + digits['5th'] == 30:
-        return True
+    return (digits['3rd'] + digits ['5th'] == 14 and \
+    digits['1st'] == 2*digits['2nd'] - 1 and \
+    digits['4th'] == digits['2nd'] + 1 and \
+    digits['2nd'] + digits['3rd'] == 10 and \
+    digits['1st'] + digits['2nd'] + digits['3rd'] + digits['4th'] + digits['5th'] == 30)
+
 
 for guesses in range(0, 100000):
     pin_temp = str (guesses).zfill(5)
@@ -16,5 +16,5 @@ for guesses in range(0, 100000):
     digits ['4th'] = int(pin_temp [3])
     digits ['5th'] = int(pin_temp [4])
     
-    if guess_is_correct(guesses):
+    if guess_is_correct(digits):
         print (guesses)
