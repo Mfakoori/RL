57a6e02aa3ee52cb609f70702038ff31f32915c4
Mohammad Hossein Fakouri <m.fakoori9776@gmail.com>
2025-04-19 21:27:21 +0330

    Update api_04.py

--- api_04.py
+++ api_04.py
@@ -13,14 +13,20 @@ sunset = sunset.strftime ('%I:%M:%S %p') # reformatting
 current_time = datetime.now()
 current_time = current_time.strftime('%I:%M:%S %p')
 
+
+def mysms (time):
+    mytext = 'hi there, you reached to sunset. current time: %s' % time
+    my_api_key = '336F786D367A426543342F4A6B51706B3335764465775735733473594F72587A444F6E57635431304E70383D'
+    url1 = 'https://api.kavenegar.com/v1/%s/sms/send.json' % my_api_key #this % my_api_key used to be replased with %s inside the string
+    mypayload = {'receptor':'09350540507', 'message':  mytext } # this %i recieves integer value from outside of the string
+    rq.post(url1, data= mypayload) # sends an sms to the phone
+
 import time
 while current_time < sunset:
-    time.sleep(60)
+    current_time = datetime.now()
+    current_time = current_time.strftime('%I:%M:%S %p') 
+    #time.sleep(300)
+    print ('    sunset: ', sunset, 'current_time: ', current_time)
 else:
+    mysms(current_time)
     print ('sunset: ', sunset, 'current_time: ', current_time)
-
-def mysms (number):
-    my_api_key = '336F786D367A426543342F4A6B51706B3335764465775735733473594F72587A444F6E57635431304E70383D'
-    url = 'https://api.kavenegar.com/v1/%s/sms/send.json' % my_api_key #this % my_api_key used to be replased with %s inside the string
-    mypayload = {'receptor':'09350540507', 'message': 'hi there, you have typed %i' % number} # this %i recieves integer value from outside of the string
-
